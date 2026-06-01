33d50fabf96f01edfbb67750579850b379499462
YASMIM RODRIGUES SANTOS SILVA <60164126007@senacrs.edu.br>
2026-06-01 17:25:55 -0300

    exercicios da aula 01/06

---
+++ app/style/proj/main.py
@@ -0,0 +1,18 @@
+def calcula_area_triangulo(base: flot,altura: float): 
+    area = (base * altura) / 2
+    print(F"A area do triangulo é: {area}")
+
+
+def converte_temperatura_fahrenreit(graus_celsius: float):
+    celcius = (fahrenreit - 32) * 5/9
+    temperatura = (graus celsius * (9/5)) - 32
+print (F" A temperatura em fahrenreit é {temperatura}")
+  
+
+
+
+
+  def converte_em_dolares (reais: float)
+  dolares = reais / 5.04
+  print (f"o valor em dolares é: {dolares}")
+  
